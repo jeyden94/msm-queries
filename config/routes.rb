e1f29c2edf8f02aa051d315a9fdc0662eec56f9f
jeyden94 <jeyden94@gmail.com>
2024-11-04 18:09:38 +0000

    Adds directors#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  #controller name should match the table i'm pulling from; Index is conventional name for an action that just displays a list of records from a table
+  get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
+
 end
